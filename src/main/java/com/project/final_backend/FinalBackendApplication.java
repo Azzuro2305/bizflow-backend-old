9e6c532e223bdc9d1abbaa0bd75eaecef9fc676c
Azzuro2305 <mr.swanhtet2305@gmail.com>
2024-03-15 13:17:16 +0630

    add spring boot frame for final project

---
+++ src/main/java/com/project/final_backend/FinalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.final_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/final_backend/FinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.final_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
