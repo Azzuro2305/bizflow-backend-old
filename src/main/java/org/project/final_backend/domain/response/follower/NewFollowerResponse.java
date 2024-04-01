367acf1c8110b869550c5eecc0b2f8e9a0e4fd73
Azzuro2305 <mr.swanhtet2305@gmail.com>
2024-04-01 09:55:33 +0630

    modify follower and following feature

---
+++ src/main/java/org/project/final_backend/domain/response/follower/NewFollowerResponse.java
@@ -0,0 +1,15 @@
+package org.project.final_backend.domain.response.follow;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class NewFollowerResponse {
+    private UUID followerId;
+    private UUID followingId;
+    private LocalDateTime followedAt;
+}
