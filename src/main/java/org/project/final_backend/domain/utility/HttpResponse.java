6ffa186a2d5a716094069c3f4d420072a8d021e5
HlaingHpone1 <hlainghpone1@gmail.com>
2024-06-12 01:04:16 +0630

    Refactor(pages): Change Exception throw string to JSON Format and Change Http Response

--- src/main/java/org/project/final_backend/domain/utility/HttpResponse.java
+++ src/main/java/org/project/final_backend/domain/utility/HttpResponse.java
@@ -1,5 +1,6 @@
 package org.project.final_backend.domain.utility;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
@@ -10,23 +11,34 @@ import java.time.LocalDateTime;
 @Setter
 public class HttpResponse<T> {
     private String message;
-    private int httpStatusCode;
-    private HttpStatus httpStatus;
-    private LocalDateTime date;
+    private boolean isSuccess;
+    private Meta meta;
     private T data;
 
-    public HttpResponse(T data, String message, HttpStatus httpStatus){
-        this.data = data;
+    public HttpResponse(T data, String message, HttpStatus httpStatus, boolean isSuccess) {
         this.message = message;
-        this.httpStatusCode = httpStatus.value();
-        this.httpStatus = httpStatus;
-        this.date = LocalDateTime.now();
+        this.isSuccess = isSuccess;
+        this.meta = new Meta(httpStatus.value());
+        this.data = data;
     }
 
-    public HttpResponse(String message, HttpStatus httpStatus){
+    public HttpResponse(String message, HttpStatus httpStatus) {
         this.message = message;
-        this.httpStatusCode = httpStatus.value();
-        this.httpStatus = httpStatus;
-        this.date = LocalDateTime.now();
+        this.isSuccess = false; // Default to false for error messages
+        this.meta = new Meta(httpStatus.value());
+    }
+
+    @Getter
+    @Setter
+    public static class Meta {
+        @JsonProperty("code")
+        private int code;
+
+        private LocalDateTime date;
+
+        public Meta(int httpStatusCode) {
+            this.code = httpStatusCode;
+            this.date = LocalDateTime.now();
+        }
     }
 }
--- src/main/java/org/project/final_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/org/project/final_backend/exception/GlobalExceptionHandler.java
@@ -1,25 +1,35 @@
 package org.project.final_backend.exception;
 
+import org.project.final_backend.domain.utility.HttpResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserFoundException.class)
-    public ResponseEntity<String> handleUserFoundException(UserFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<HttpResponse<Void>> handleUserFoundException(UserFoundException ex) {
+        HttpResponse<Void> response = new HttpResponse<>(null, ex.getMessage(), HttpStatus.BAD_REQUEST, false);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<HttpResponse<Void>> handleUserNotFoundException(UserNotFoundException ex) {
+        HttpResponse<Void> response = new HttpResponse<>(null, ex.getMessage(), HttpStatus.NOT_FOUND, false);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<String> handleInvalidPasswordException(InvalidPasswordException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<HttpResponse<Void>> handleInvalidPasswordException(InvalidPasswordException ex) {
+        HttpResponse<Void> response = new HttpResponse<>(null, ex.getMessage(), HttpStatus.UNAUTHORIZED, false);
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
